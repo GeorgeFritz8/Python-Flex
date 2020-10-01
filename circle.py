c287536d491289b533e6e70164ff507bf62e2ea2
GeorgeFritz8 <70896922+GeorgeFritz8@users.noreply.github.com>
2020-10-01 14:12:01 +0200

    Add files via upload
    "Turtle opdracht toegevoegd"

---
+++ circle.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.left(180)
+turtle.forward(125)
+turtle.right(90)
+turtle.forward(125)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(180)
+turtle.forward(100)
+turtle.right(450)
+turtle.forward(125)
+turtle.right(90)
+turtle.forward(125)
+
+print("Tadaaaa!")
